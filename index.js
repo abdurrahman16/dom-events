690c0897714e038aa41706fbcdbea8f37e9d1c33
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-23 23:49:17 +0600

    js dom

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert("hello");
+var authorModel = document.getElementsByClassName('author');
+
+for ( var i=0; i<authorModel.length; i++){
+    var element =authorModel[i];
+    element.style.backgroundColor ='teal';
+    element.innerHTML = 'Writer- '+ i;
+    console.log(element);
+}
