ae1d04b6b370690b186be3de2930fd8c4772e943
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-24 09:29:14 +0600

    create element

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-var authorModel = document.getElementsByClassName('author');
+// var authorModel = document.getElementsByClassName('author');
 
-for ( var i=0; i<authorModel.length; i++){
-    var element =authorModel[i];
-    element.style.backgroundColor ='teal';
-    element.innerHTML = 'Writer- '+ i;
-    console.log(element);
-}
+// for ( var i=0; i<authorModel.length; i++){
+//     var element =authorModel[i];
+//     element.style.backgroundColor ='teal';
+//     element.innerHTML = 'Writer- '+ i;
+//     console.log(element);
+// }
+
+// const article = document.getElementsById('special');
+// // console.log(article);
+// const newParagraph = document.createElement('p');
+// newParagraph.innerHTML ='Java Script do this';
+// article.appendChild(newParagraph);
+
+
+const ul = document.getElementById('item-list');
+const li = document.createElement('li');
+li.innerHTML = 'babu';
+ul.appendChild(li);
